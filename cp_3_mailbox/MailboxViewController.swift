655514735c4f221b8328e0c8c89ac47f537a4255
cheng <gnuchex@gmail.com>
2015-11-14 15:39:51 -0800

    single message pan working

--- cp_3_mailbox/MailboxViewController.swift
+++ cp_3_mailbox/MailboxViewController.swift
@@ -9,30 +9,56 @@
 import UIKit
 
 class MailboxViewController: UIViewController {
-
+    
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var singleMessageTrayView: UIView!
+    var singleMessageTrayOriginalCenter: CGPoint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         scrollView.contentSize = CGSize(width: 320, height: 1400)
-
-        // Do any additional setup after loading the view.
+        singleMessageTrayOriginalCenter = singleMessageTrayView.center
+        
+        
     }
-
+    
+    @IBAction func didPanSingleMessage(panGestureRecognizer: UIPanGestureRecognizer) {
+        let point = panGestureRecognizer.locationInView(view) //the finger position
+        let translation = panGestureRecognizer.translationInView(view) //the relavent movement
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            print("Gesture began at: \(point)")
+            
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed {
+            print("Gesture changed at: \(point)")
+            singleMessageTrayView.center = CGPoint(x: singleMessageTrayOriginalCenter.x + translation.x, y: singleMessageTrayOriginalCenter.y)
+            
+        }
+        else if panGestureRecognizer.state == UIGestureRecognizerState.Ended {
+            
+            print("Gesture ended at: \(point)")
+        }
+        
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
